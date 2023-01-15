f3f71a589ff913826117a02e4357713e28be55e5
Kirenai <pool_dota2@hotmail.com>
2023-01-15 13:39:42 -0500

    Add nourishment principal test class

---
+++ nourishment/src/test/java/me/kirenai/re/nourishment/NourishmentApplicationTests.java
@@ -0,0 +1,12 @@
+package me.kirenai.re.nourishment;
+
+import org.junit.jupiter.api.Test;
+
+//@SpringBootTest
+class NourishmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
